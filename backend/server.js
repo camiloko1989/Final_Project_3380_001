03eeb2e549c9ed967273048078d97be8bcb4bc97
BillyLam821 <BillyLam821@gmail.com>
2023-04-10 11:39:05 -0700

    resolved map icon & some css

--- backend/server.js
+++ backend/server.js
@@ -16,16 +16,16 @@ app.use("/api/facility", require("./routes/facilityRoutes"));
 app.use("/api/comments", require("./routes/commentRouter"));
 
 // Serve frontend
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../finalproject3600/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../finalproject3600/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, "../", "finalproject3600", "build", "index.html")
-//     )
-//   );
-// } else {
-//   app.get("/", (req, res) => res.send("Please set to production"));
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "finalproject3600", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
